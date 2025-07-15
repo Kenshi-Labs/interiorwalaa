611d46def409528ca0757ca0249a6715efa3d473
vinaykenshilabs <anil@kenshilabs.com>
2025-07-15 11:37:37 +0530

    intial setup and footer done

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { FooterSubsection } from "@/components/partials/Footer";
+
+
+export default function Home() {
+  return (
+<div>
+  <h1>Hello World</h1>
+  <FooterSubsection />
+</div>
+  );
+}
