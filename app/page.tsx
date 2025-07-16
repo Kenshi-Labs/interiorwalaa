87213ce9ff970aaf661c4fd5c2f051b93cc01a2c
Barath Muthuvel <baratheabs4@gmail.com>
2025-07-16 19:02:51 +0530

    ui fix

--- app/page.tsx
+++ app/page.tsx
@@ -15,25 +15,25 @@ import WhyChooseUs from "@/components/whyChooseUs";
 export default function Home() {
   return (
     <div className="bg-[var(--color-white-alt)]">
-    
+
       <HeroBanner
         backgroundImage="https://interiorwalaa.smepulse.in/homebannerbg.png"
       />
-      <AllAbout 
-  layout="image-left"
-  title="All About"
-  subtitle="Interiorwalaa"
-  description={[
-    "Interiorwalaa stands at the forefront of commercial interior designers in Bangalore. With six years of industry leadership and more than 50 spaces transformed nationwide, we excel in designing interiors that resonate with quality and distinction. ",
-    "Our experienced team is well-versed in creating dynamic environments for cafes, pubs, restaurants, and hotels, each designed to enhance functionality and style."
-  ]}
-  images={{
-    img1: "https://interiorwalaa.smepulse.in/allaboutimg1.png",
-    img2: "https://interiorwalaa.smepulse.in/allaboutimg2.png", 
-    img3: "https://interiorwalaa.smepulse.in/allaboutimg3.png"
-  }}
-  buttonText="Get In Touch"
-/>
+      <AllAbout
+        layout="image-left"
+        title="All About"
+        subtitle="Interiorwalaa"
+        description={[
+          "Interiorwalaa stands at the forefront of commercial interior designers in Bangalore. With six years of industry leadership and more than 50 spaces transformed nationwide, we excel in designing interiors that resonate with quality and distinction. ",
+          "Our experienced team is well-versed in creating dynamic environments for cafes, pubs, restaurants, and hotels, each designed to enhance functionality and style."
+        ]}
+        images={{
+          img1: "https://interiorwalaa.smepulse.in/allaboutimg1.png",
+          img2: "https://interiorwalaa.smepulse.in/allaboutimg2.png",
+          img3: "https://interiorwalaa.smepulse.in/allaboutimg3.png"
+        }}
+        buttonText="Get In Touch"
+      />
       <WhatWeOffer />
       <WhyChooseUs />
       <ServicesPortfolio />
@@ -43,7 +43,7 @@ export default function Home() {
       <VideoGallerySection />
       <TestimonialCarousel />
       <ContactSection />
-     
+
     </div>
   );
 }
