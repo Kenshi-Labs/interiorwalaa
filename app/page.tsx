442c7ff6ed2f6cf2197f7feef3b3e0bfcbad4b6f
vinaykenshilabs <anil@kenshilabs.com>
2025-07-16 13:05:57 +0530

    scroll page developed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+'use client'
 import AllAbout from "@/components/common/AllAbout";
 import { HeroBanner } from "@/components/common/HeroBanner";
 import ContactSection from "@/components/contact";
 import FAQSection from "@/components/FAQSection";
 import { FooterSubsection } from "@/components/partials/Footer";
 import { Header } from "@/components/partials/Header";
+import TestimonialCarousel from "@/components/scrollToSlide";
 import ScrollWrapper from "@/components/scrollWrapper";
 import ServicesPortfolio from "@/components/servicesPortfolio";
 import WhatWeOffer from "@/components/whatweoffer";
@@ -22,6 +24,7 @@ export default function Home() {
       <ScrollWrapper />
       <ServicesPortfolio />
       <FAQSection />
+      <TestimonialCarousel />
       <ContactSection/>
       <FooterSubsection />
     </div>
